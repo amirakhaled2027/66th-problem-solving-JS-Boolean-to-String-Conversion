a6a7314331609e4c92f628e8af252c40e2b4c0fa
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-07-29 23:41:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// Create a function that takes a boolean variable flag and returns it as a string.
+
+function boolToString(flag) {
+    if (flag === true) {
+        return "true";
+    } else {
+        return "false";
+    }
+}
+console.log(returnString(true));
+console.log(returnString(false));
